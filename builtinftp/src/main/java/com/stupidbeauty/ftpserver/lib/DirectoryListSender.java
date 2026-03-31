acb2fee5c989953c3ea27dbaef74b61fd4d977de
hxcan <75723435+hxcan@users.noreply.github.com>
2026-03-31 14:07:16 +0800

    fix: move placeholderLine variable declaration outside if block to fix compilation error
    - Declare placeholderLine before the if block so it's accessible in Log.d()
    - Fixes: error: cannot find symbol variable placeholderLine

--- builtinftp/src/main/java/com/stupidbeauty/ftpserver/lib/DirectoryListSender.java
+++ builtinftp/src/main/java/com/stupidbeauty/ftpserver/lib/DirectoryListSender.java
@@ -229,9 +229,9 @@ public class DirectoryListSender
         controlConnectHandler.checkFileManagerPermission(Constants.Permission.Read, null); // 检查权限
 
         // 👇 新增：如果启用了 Dolphin bug #474238 的绕过选项，则插入一个占位文件
+        String placeholderLine = "-rw-r--r-- 1 user group 0 Jan 01 00:00 .dolphin_placeholder\r\n";
         if (isEnableDolphinBug474238Placeholder())
         {
-          String placeholderLine = "-rw-r--r-- 1 user group 0 Jan 01 00:00 .dolphin_placeholder\r\n";
           binaryStringSender.sendStringInBinaryMode(placeholderLine);
         }
           Log.d(TAG, "DirectoryListSender [Empty Dir], sending placeholder line: [" + placeholderLine + "]"); // Debug
